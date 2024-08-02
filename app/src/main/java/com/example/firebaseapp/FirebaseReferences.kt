1f99ad34dd289098653d2698a455f498f2210710
Jaime Rodriguez Dennis <jairoden181203@gmail.com>
2024-08-01 18:58:48 -0700

    Finish commit

---
+++ app/src/main/java/com/example/firebaseapp/FirebaseReferences.kt
@@ -0,0 +1,9 @@
+package com.example.firebaseapp
+
+class FirebaseReferences {
+    companion object {
+        const val DB_URL = "https://contactos-4d77f-default-rtdb.firebaseio.com/"
+        const val DB_NAME = "contactos"
+        const val TABLE_NAME = "contacts"
+    }
+}
---
+++ app/src/main/java/com/example/firebaseapp/ListActivity.kt
@@ -0,0 +1,123 @@
+package com.example.firebaseapp
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import com.example.firebaseapp.Contacts
+import com.example.firebaseapp.FirebaseReferences
+
+class ListActivity : AppCompatActivity() {
+
+    private lateinit var basedatabase: FirebaseDatabase
+    private lateinit var referencia: DatabaseReference
+    private lateinit var btnNuevo: Button
+    private lateinit var listViewContactos: ListView // Obtén una referencia a tu ListView
+    private val context: Context = this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        basedatabase = FirebaseDatabase.getInstance()
+        referencia = basedatabase.getReferenceFromUrl(
+            FirebaseReferences.DB_URL + "/" + FirebaseReferences.DB_NAME + "/" + FirebaseReferences.TABLE_NAME
+        )
+        btnNuevo = findViewById(R.id.btnNew)
+        listViewContactos = findViewById(R.id.listViewContactos) // Asocia el ListView con el XML
+        obtenerContactos()
+        btnNuevo.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+    }
+
+    private fun obtenerContactos() {
+        val contactos = ArrayList<Contacts>()
+        val listener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+                val contacto = dataSnapshot.getValue(Contacts::class.java)
+                contacto?.let {
+                    contactos.add(it)
+                    val adapter = MyArrayAdapter(context, R.layout.layout_contact, contactos)
+                    listViewContactos.adapter = adapter // Establece el adaptador en el ListView
+                }
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {}
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+            override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {}
+            override fun onCancelled(databaseError: DatabaseError) {}
+        }
+        referencia.addChildEventListener(listener)
+    }
+
+    inner class MyArrayAdapter(
+        context: Context,
+        private val textViewRecursoId: Int,
+        private val objects: ArrayList<Contacts>
+    ) : ArrayAdapter<Contacts>(context, textViewRecursoId, objects) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = convertView ?: layoutInflater.inflate(this.textViewRecursoId, null)
+            val lblNombre = view.findViewById<TextView>(R.id.lblNombreContacto)
+            val lblTelefono = view.findViewById<TextView>(R.id.lblTelefonoContacto)
+            val btnModificar = view.findViewById<Button>(R.id.btnModificar)
+            val btnBorrar = view.findViewById<Button>(R.id.btnBorrar)
+
+            if (objects[position].favorite > 0) {
+                lblNombre.setTextColor(Color.BLUE)
+                lblTelefono.setTextColor(Color.BLUE)
+            } else {
+                lblNombre.setTextColor(Color.BLACK)
+                lblTelefono.setTextColor(Color.BLACK)
+            }
+            lblNombre.text = objects[position].name
+            lblTelefono.text = objects[position].phoneNumber1
+
+            btnBorrar.setOnClickListener {
+                mostrarDialogoConfirmacion(objects[position], position)
+            }
+
+            btnModificar.setOnClickListener {
+                val oBundle = Bundle()
+                oBundle.putSerializable("contacto", objects[position])
+                val intent = Intent()
+                intent.putExtras(oBundle)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+
+            return view
+        }
+    }
+
+    private fun mostrarDialogoConfirmacion(contacto: Contacts, position: Int) {
+        val builder = AlertDialog.Builder(this@ListActivity)
+        builder.setTitle("Confirmar eliminación")
+        builder.setMessage("¿Estás seguro de que quieres eliminar este contacto?")
+        builder.setPositiveButton("Sí") { dialog, _ ->
+            borrarContacto(contacto.id)
+            (listViewContactos.adapter as? MyArrayAdapter)?.remove(contacto) // Elimina el contacto del adaptador
+            Toast.makeText(context, "Contacto eliminado con éxito", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("No") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun borrarContacto(childIndex: String) {
+        referencia.child(childIndex).removeValue()
+    }
+}
